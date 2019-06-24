31e413d35b2cfe145b46a0e1b67e1a723dd45c9d
[your <leigh.tm@gmail.com>
2019-06-24 18:12:35 -0400

    working on log on

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/newdb.db3',
+      filename: './data/AMusers.db3',
     },
     useNullAsDefault: true, 
   },
