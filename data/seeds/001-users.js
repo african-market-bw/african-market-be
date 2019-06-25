76e58a1228bc1f6fbafbc7152d1f5470b60f60ef
[your <leigh.tm@gmail.com>
2019-06-25 13:53:35 -0400

    deploy time part 3

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users').truncate()
-    .then(function () {
+  // return knex('users')
+  //   .then(function () {
       // Inserts seed entries
       return knex('users').insert([
         {name:'Ashanti', username:'username', password:'password',email:'someone@email.com'},
         {name:'Awiti', username:'username1', password:'password',email:'someone@email.com'},
         {name:'Afua', username:'username2', password:'password', email:'someone@email.com'},
       ]);
-    });
+   
 };
